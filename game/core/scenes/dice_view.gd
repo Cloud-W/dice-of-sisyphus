0bb70803783475c9087cfbf83dbf1f93eab1d60b
‘suoyike’ <494311870@qq.com>
2025-04-15 16:21:11 +0800

    修复：连续点击roll按钮导致意外扔出骰子的bug

--- game/core/scenes/dice_view.gd
+++ game/core/scenes/dice_view.gd
@@ -6,15 +6,25 @@ signal on_dice_roll(result: int)
 
 @onready var animation: AnimatedSprite2D = %AnimatedSprite2D
 
+var _can_roll: bool = true
+
 
 func _ready() -> void:
 	pass
 
 
 func roll() -> void:
+	if not _can_roll:
+		return
+	_can_roll = false
+	
 	var result: int = randi_range(0, 5)
 	animation.play("roll");
 	await get_tree().create_timer(anim_time).timeout
 	animation.play("idle");
 	animation.frame = result
 	on_dice_roll.emit(result + 1)
+
+
+func _on_roll_completed() -> void:
+	_can_roll = true
