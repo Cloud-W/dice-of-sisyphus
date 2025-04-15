cefb7a978c6354abc3bf0008a31a224c05a97b94
‘suoyike’ <494311870@qq.com>
2025-04-15 16:16:32 +0800

    新增：自动扔骰子

--- game/core/scenes/dice_view.gd
+++ game/core/scenes/dice_view.gd
@@ -1,19 +1,20 @@
-class_name DiceView extends Node2D
+class_name DiceView
+extends Node2D
 
-signal on_dice_roll(result : int)
+signal on_dice_roll(result: int)
+@export var anim_time: float = 1.5
 
-@export var anim_time : float = 1.5
+@onready var animation: AnimatedSprite2D = %AnimatedSprite2D
 
-@onready var animation : AnimatedSprite2D = %AnimatedSprite2D
 
 func _ready() -> void:
-    pass
-    
+	pass
+
 
 func roll() -> void:
-    var result : int = randi_range(0, 5)
-    animation.play("roll");
-    await get_tree().create_timer(anim_time).timeout
-    animation.play("idle");
-    animation.frame = result
-    on_dice_roll.emit(result + 1)
+	var result: int = randi_range(0, 5)
+	animation.play("roll");
+	await get_tree().create_timer(anim_time).timeout
+	animation.play("idle");
+	animation.frame = result
+	on_dice_roll.emit(result + 1)
--- game/core/scenes/main.gd
+++ game/core/scenes/main.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+signal move_pawn_completed
+signal roll_requested
 @export var _grid: Grid
 @export var _pawn: Pawn
+@export var _is_auto_roll : bool
+
 
 @onready var _grid_view: GridView = %GridView
 @onready var _pawn_agent: PawnAgent = %PawnAgent
@@ -27,8 +31,12 @@ func _move_pawn(step: int):
 		path_positions.append(_grid_view.get_cell_position(i))
 
 	await _pawn_agent.move_path(path_positions)
-	
+
 	_enter_current_cell()
+	move_pawn_completed.emit()
+	
+	if _is_auto_roll: 
+		roll_requested.emit()
 
 
 func _enter_current_cell():
